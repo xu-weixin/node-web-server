3893ab857744de5db03bac7c0eb34f959e85da62
xu-weixin <zealingstar@163.com>
2018-01-06 16:12:12 +0800

    Add some pages

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+app.get('/', (req, res) =>
+  res.render('home.hbs', {
+    pageTitle: 'Home'
+  })
+);
+
+app.get('/about', (req, res) =>
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  })
+);
+
+app.get('/products', (req, res) =>
+  res.render('products.hbs', {
+    pageTitle: 'Products'
+  })
+);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
